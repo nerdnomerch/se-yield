370e0f464d7aaceec6e81f2eba0569268397332e
NERD777 <nugrohoandi343@gmail.com>
2025-05-24 10:28:20 +0700

    first commit

---
+++ seyield-frontend/app/config/contract-addresses.ts
@@ -0,0 +1,19 @@
+/**
+ * Contract addresses for the SEI Testnet
+ * Updated from deployment on May 2024
+ */
+export const contractAddresses = {
+  // Token contracts
+  usdc: '0x953e5610c73c989fe7c75d3d67be0a1e44a8e797',
+  principalToken: '0x13cf4e3e284d34c575ceeccb0791ca535a657da2',
+  yieldToken: '0xf12cd252ca50781ec88c2d8832ca4f9c4bf11d82',
+
+  // Core protocol contracts
+  treasury: '0xd2b0838ff0818e9aa185a712576cb3ee0885deda',
+  yieldProtocol: '0x0d5cb2123da148b83872c6977265b3a77b2e208d',
+  fundsVault: '0xe5f56371f2c27cbbd3dd8eecedcd3b53847c6b05',
+
+  // Auxiliary contracts
+  faucet: '0x0814c0e18f8a275f58a4a74aa5183f9e0dd324e6',
+  merchant: '0x7456733cb8d301cbee45c89e0aeb46edda511e7e',
+}
